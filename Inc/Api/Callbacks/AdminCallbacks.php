6e4bc47a9d238c31193b9d9446455b6f8ddfdb61
jkip-21 <jonahkapkory1538@gmail.com>
2023-02-27 10:35:43 +0300

    Adding callbacks

--- Inc/Api/Callbacks/AdminCallbacks.php
+++ Inc/Api/Callbacks/AdminCallbacks.php
@@ -1,30 +1,19 @@
 <?php
 /**
- * @package  MeekPlugin
+ * @package  EmployeePlugin
  */
 
 namespace Inc\Api\Callbacks;
 use Inc\Base\BaseController;
 
 class AdminCallbacks extends BaseController{
-    public function MeekMain(){
-        return require_once("$this->plugin_path/templates/meekinputmembers.php");
+    public function employeeDasboard(){
+        return require_once("$this->plugin_path/templates/Employee.php");
     }
-    public function Cohort9Main(){
-        return require_once("$this->plugin_path/templates/meekviewmembers.php");
+    public function newEmployeeMain(){
+        return require_once("$this->plugin_path/templates/NewEmployee.php");
     }
-    public function meekOptionsGroup($input){
-        return $input;
-    }
-    public function meekAdminSection(){
-        echo 'This is the first section';
-    }
-    public function meekTextExample(){
-        $value = esc_attr(get_option('meek_example'));
-        echo '<input type="text" class="regular-text" name="meek_example" value="'.$value.'" placeholder="Write Your First Name"';
-    }
-    public function meekSecondName(){
-        $value = esc_attr(get_option('second_name'));
-        echo '<input type="text" class="regular-text" name="second_name" value="'.$value.'" placeholder="Write Your Second Name"';
+    public function employeeDataMain(){
+        return require_once("$this->plugin_path/templates/EmployeesData.php");
     }
 }
