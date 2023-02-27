d73d226412552557c9e5d39444de667658ad96ab
jkip-21 <jonahkapkory1538@gmail.com>
2023-02-27 10:23:25 +0300

    Add new employee

---
+++ templates/NewEmployee.php
@@ -0,0 +1,23 @@
+<h2>Add New Employee</h2>
+<div class="row justify-content-center">
+<form action="" method="post" style="width:40vw; box-shadow:3px 3px 3px 3px 3px grey; padding:30px;">
+<div class="form-group">
+    <input type="text" name="username" id="username" placeholder="User Name *" class="form-control input-sm mb-4" required>
+</div>
+<div class="form-group">
+    <input type="email" name="email" id="email" placeholder="User Email *" class="form-control input-sm mb-4" required>
+</div>
+<div class="form-group">
+    <input type="text" name="phone" id="phone" placeholder="Phone Number" class="form-control input-sm mb-4" required>
+</div>
+<div class="form-group">
+    <input type="text" name="password" id="password" placeholder="*****" class="form-control input-sm mb-4" required>
+</div>
+<div class="row justify-content-center">
+    <div class="
+    col-xs-4 col-sm-4 col-md-4">
+<input type="submit" value="Send" name="submitdata" class="btn btn-success btnblock">
+</div>
+</div>
+</form>
+</div>
